fc087fe611a473507e03fb7ca15f6842484e485e
Kolyan99 <shuknikolay99@gmail.com>
2022-11-23 12:31:01 +0300

    Homework Lesson18

--- app/src/main/java/com/example/androidprojecthomework/MainActivity.kt
+++ app/src/main/java/com/example/androidprojecthomework/MainActivity.kt
@@ -1,50 +1,62 @@
 package com.example.androidprojecthomework
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.RadioButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.cardview.widget.CardView
 import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // val layoutName = findViewById<TextInputLayout>(R.id.ex_text)
-        // val layoutPassword = findViewById<TextInputLayout>(R.id.ex_text2)
+        val editText = findViewById<EditText>(R.id.ed_text1)
+        val editText2 = findViewById<EditText>(R.id.ed_text2)
+        val btn = findViewById<Button>(R.id.btn)
+        val card_View = findViewById<CardView>(R.id.card)
+        val text_View = findViewById<TextView>(R.id.text_view)
+        val rb1 = findViewById<RadioButton>(R.id.rb_1)
+        val rb2 = findViewById<RadioButton>(R.id.rb_2)
+        val iv_image = findViewById<ImageView>(R.id.iv_image)
 
-        val editText = findViewById<EditText>(R.id.ex_text)
-        val editText2 = findViewById<EditText>(R.id.ex_text2)
 
-        val button = findViewById<Button>(R.id.btn_display)
-        val textView = findViewById<TextView>(R.id.tv_text)
-
-        val dialog = AlertDialog.Builder(this)
-            .setTitle("Information")
-            .setMessage(getString(R.string.info))
-            .setCancelable(false)
-            .setPositiveButton("OK") { dialog, _ ->
-                Toast.makeText(this, getString(R.string.text), Toast.LENGTH_SHORT).show()
-            }
-            .setNegativeButton("Cancle") { dialog, _ ->
-                dialog.cancel()
-            }
-
-        button.setOnClickListener {
-            dialog.show()
-
-            if (editText.text.toString().isEmpty()) {
-                editText.error = "No name "
-            } else if (editText2.text.toString().isEmpty()) {
-                editText2.error = "No password "
+        rb1.setOnClickListener {
+            if(rb1.isChecked){
+                rb2.isChecked = false
+            }else{
+                rb1.isChecked = true
             }
         }
+
+        rb2.setOnClickListener {
+            if(rb2.isChecked){
+                rb1.isChecked = false
+            }else{
+                rb2.isChecked = true
+            }
+        }
+
+        btn.setOnClickListener {
+            if(editText.text.toString().isEmpty()){
+                editText.error = "email can't be empty"
+            }else if(editText2.text.toString().isEmpty()){
+                editText2.error = "passworld can't be empty"
+
+            }
+        }
+
     }
+
 }
 
 
